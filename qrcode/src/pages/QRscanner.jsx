ae6f0a69c579521748c669746a9c0c9b92443e3e
Thulio Balbuena <thuliobalbuena@hotmail.com>
2024-05-27 20:50:19 -0300

    arrumar y

--- qrcode/src/pages/QRscanner.jsx
+++ qrcode/src/pages/QRscanner.jsx
@@ -189,7 +189,7 @@ function QRscanner() {
               var qrImageData = canvas.toDataURL("image/png");
           
               // Add the QR Code image to the PDF
-              doc.addImage(qrImageData, "PNG", 80, 19, 16, 16);
+              doc.addImage(qrImageData, "PNG", 80,20.5, 17, 17);
             });
           }
           }
