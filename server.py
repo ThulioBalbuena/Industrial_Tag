cc597194aaf3fb53d89109801fd8bc9627d26229
Thulio Balbuena <thuliobalbuena@hotmail.com>
2024-09-24 22:53:19 -0300

    update mongovalidation

--- server.py
+++ server.py
@@ -1,37 +1,42 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from bson.json_util import dumps
-from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)  # Habilitar CORS
-
 
 # Configurar a conexão com MongoDB usando uma variável de ambiente
 mongo_uri = os.getenv("MONGO_URI", "mongodb+srv://thulio:admin@cluster0.mongodb.net/TICKETMASTER?retryWrites=true&w=majority")
 client = MongoClient(mongo_uri)
 db = client["TICKETMASTER"]
 collection = db["Stock"]
-@app.route('/')
-def index():
-    return "Backend Flask está rodando!", 200
 
 # Rota para salvar dados do QR Code
 @app.route('/api/qrcodes', methods=['POST'])
 def save_qrcode():
     try:
         data = request.json
-        # Verificar se já existe um documento com o mesmo código
-        existing_data = collection.find_one({"codigo": data['codigo']})
+        if not data:
+            return jsonify({"message": "Dados inválidos recebidos."}), 400
+        
+        print("Dados recebidos:", data)  # Log dos dados recebidos
+
+        # Verificar se já existe um documento com os mesmos campos "codigo", "descricao" e "quantidade"
+        existing_data = collection.find_one({
+            "codigo": data['codigo'],
+            "descricao": data['descricao'],
+            "quantidade": data['quantidade'],
+            "localizacao": data['localizacao'],
+            "lote": data['lote']
+        })
         
         if existing_data:
-            return jsonify({"message": "Dados já existem no banco de dados."}), 400  # Retorna erro se o dado já existir
+            return jsonify({"message": "Dados já existem no banco de dados."}), 400  # Retorna erro se todos os dados coincidirem
         
         # Se não existir, insere os dados
         collection.insert_one(data)
         return jsonify({"message": "Dados do QR Code salvos com sucesso!"}), 200
     except Exception as e:
+        print("Erro ao salvar os dados:", str(e))  # Log de erro
         return jsonify({"error": str(e)}), 500
 
 # Rota para obter todos os dados
@@ -41,6 +46,7 @@ def get_qrcodes():
         qrcodes = collection.find()
         return dumps(qrcodes), 200
     except Exception as e:
+        print("Erro ao buscar os dados:", str(e))  # Log de erro
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
